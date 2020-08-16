eac8c42358e3efe77f8cbec5d08153df7b7c17ee
addRog <acrogers.ds@gmail.com>
2020-08-16 14:15:31 -0400

    Implement data pulling for all tables but subjects
    Signed-off-by: addRog <acrogers.ds@gmail.com>

--- get_data.py
+++ get_data.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 import argparse
-
-wk_prefix = "https://api.wanikani.com/v2/"
+import time
+import tqdm
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -14,42 +14,70 @@ class BearerAuth(requests.auth.AuthBase):
         return r
 
 
-# TODO: introduce mechanism to avoid exceeding request limits
 def pull_data(session: requests.sessions, token: str, url: str):
-    headers = {"Wanikani-Revision": "20170710"}
+    """
+    Pulls data using the WaniKani RESTful API for an associated user and the collection/resource specified by the url.
+    :param session: Requests session object, potential shared by different resources (see process_ functions).
+    :param token: Authentication token associated with the user.
+    :param url: URL suffix associated with the desired collection or resource (e.g. reviews table, kanji info, etc.)
+    :return: Yields dictionary objects (parsed json) containing collection/resource data.
+    """
+    headers = {"Wanikani-Revision": "20170710"}  # May need to modify if api changes.
     first_page = session.get(url=url, headers=headers, auth=BearerAuth(token)).json()
     yield first_page['data']
+    time.sleep(1)
 
     # cursor based pagination
     next_url = first_page['pages']['next_url']
     while next_url is not None:
         next_page = session.get(url=next_url, headers=headers, auth=BearerAuth(token)).json()
         yield next_page['data']
+        next_url = next_page['pages']['next_url']
+        time.sleep(1)
 
 
-# TODO: test this
-def process_review_statistics(session: requests.sessions, token: str) -> pd.DataFrame:
-    url = wk_prefix + 'review_statistics'
+# TODO: fix for subject
+def process_collection(session: requests.sessions, token: str, url: str) -> pd.DataFrame:
+    """
+    Processes collection
+    :param session:
+    :param token:
+    :param url:
+    :return:
+    """
     temp = []
     for page in pull_data(session, token=token, url=url):
         for item in page:
-            data = item['data']
-            data['data_updated_at'] = item['data_updated_at']
-            temp.append(data)
+            if 'subjects' not in url:
+                data = item['data']
+                data['data_updated_at'] = item['data_updated_at']
+                temp.append(data)
+            # else:
+            #     if item['data']['object'] == 'radical':
+            #         asi = item['data']['data']['amalgamation_subject_ids']
+            #         aux = item['data']['data']['auxiliary_meanings']
+            #     meanings = item['data']['data']['meanings']
+            #     readings = item['data']['data']['readings']
 
-    rev_stats_df = pd.DataFrame(temp)
-    return rev_stats_df
+    df = pd.DataFrame(temp)
+    return df
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--token", "-t", help="provide API token")
+    parser.add_argument("--token", "-t", help="user authentication token")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
     wk_token = args.token
+    wk_prefix = "https://api.wanikani.com/v2/"
+    suffixes = ['review_statistics', 'assignments', 'level_progressions', 'resets', 'reviews', 'subjects']
     sess = requests.session()
 
-    review_df = process_review_statistics(sess, wk_token)
+    for i in tqdm.tqdm(suffixes):
+        collection_df = process_collection(sess, wk_token, wk_prefix + i)
+        collection_df.to_csv(path_or_buf='data/{}.csv'.format(i), index=False)
+
+    sess.close()
