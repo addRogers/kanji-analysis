84eeaaec0729e19d969b233ea67cc41e6a11089e
addRog <69536651+addrogers@users.noreply.github.com>
2020-08-29 18:04:44 -0400

    Reviews time series merge
    Signed-off-by: addRog <69536651+addRogers@users.noreply.github.com>

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ reviews_df = pd.read_csv('data/reviews.csv')
 reviews_df['created_at'] = pd.to_datetime(reviews_df['created_at'])
 stats_df['created_at'] = pd.to_datetime(stats_df['created_at'])
 stats_df = stats_df[(stats_df['created_at'] > '2019')]
+reviews_df = reviews_df[(reviews_df['created_at'] > '2019')]
 # %%
 g = sns.FacetGrid(stats_df, col='subject_type')
 g.map(sns.distplot, 'percentage_correct', rug=True, hist=False)
@@ -21,41 +22,45 @@ sns.catplot(x='subject_type', y='percentage_correct', kind='boxen',
             data=stats_df)
 plt.show()
 # %%
-stats_df['hour'] = stats_df['created_at'].dt.hour
-stats_df['day'] = stats_df['created_at'].dt.day
-stats_df['month'] = stats_df['created_at'].dt.month
-stats_df['year'] = stats_df['created_at'].dt.year
-stats_df['weekday'] = stats_df['created_at'].dt.weekday
+reviews_df['hour'] = reviews_df['created_at'].dt.hour
+reviews_df['day'] = reviews_df['created_at'].dt.day
+reviews_df['month'] = reviews_df['created_at'].dt.month
+reviews_df['year'] = reviews_df['created_at'].dt.year
+reviews_df['weekday'] = reviews_df['created_at'].dt.weekday
 
-stats_df.groupby('hour').count()
+reviews_df.groupby('hour').count()
 
 # %%
 
-sel_hours = stats_df['hour'] >= 7
-ax = sns.relplot(x='hour', y='percentage_correct', kind='line',
-                 hue='subject_type', style='subject_type', data=stats_df[sel_hours])
+rdf = reviews_df.merge(right=stats_df[['subject_id', 'subject_type']], on='subject_id')
+rdf['total_incorrect'] = rdf['incorrect_meaning_answers'] + rdf['incorrect_reading_answers']
+sel_hours = rdf['hour'] >= 7
+ax = sns.relplot(x='hour', y='total_incorrect', kind='line',
+                 hue='subject_type', style='subject_type', data=rdf[sel_hours])
 plt.show()
+
 # %%
 
-g = sns.catplot(x='hour', y='percentage_correct',
+g = sns.catplot(x='hour', y='total_incorrect',
                 col='subject_type',
-                data=stats_df, kind='boxen')
+                data=rdf, kind='boxen')
+plt.show()
 
-p = sns.FacetGrid(stats_df, col='subject_type')
+p = sns.FacetGrid(rdf, col='subject_type')
 p.map(sns.distplot, 'hour', rug=True, hist=False)
 plt.show()
 
 # %%
 
-temp = stats_df.groupby(['weekday', 'month']).mean().reset_index().pivot('weekday', 'month', 'percentage_correct')
-sns.heatmap(annot=True, data=temp)
+temp = rdf.groupby(['weekday', 'month']).mean().reset_index().pivot('weekday', 'month', 'total_incorrect')
+sns.heatmap(annot=False, data=temp)
 plt.show()
 # %%
 
-sns.catplot(x='month', hue='year', col='subject_type', kind='count', data=stats_df)
-sns.catplot(x='hour', col='subject_type', kind='count', data=stats_df)
-sns.catplot(x='hour', kind='count', data=stats_df)
-sns.catplot(x='weekday', kind='count', data=stats_df)
+sns.catplot(x='month', hue='year', col='subject_type', kind='count', data=rdf)
+sns.catplot(x='hour', col='subject_type', kind='count', data=rdf)
+sns.catplot(x='hour', kind='count', data=rdf)
+sns.catplot(x='weekday', kind='count', data=rdf)
 plt.show()
 # %%
 
@@ -64,11 +69,9 @@ sns.clustermap(data=temp)
 plt.show()
 # %%
 
-sns.catplot(x='meaning_incorrect', kind='count', hue='subject_type', data=stats_df)
+sns.catplot(x='total_incorrect', kind='count', hue='subject_type', data=rdf)
 plt.show()
-# %%
-sns.catplot(x='reading_incorrect', kind='count', hue='subject_type', data=stats_df)
-plt.show()
+
 # %%
 # join subjects table on review statistics
 
@@ -80,8 +83,19 @@ merged_df = stats_df.merge(subject_df.drop('data_updated_at', axis=1), left_on=[
                            how='inner').drop(['hidden', 'id'], axis=1)
 
 merged_df['subject_type'] = merged_df['subject_type'].astype('category')
-merged_df['weekday'] = merged_df['weekday'].astype('category')
 merged_df['level'] = merged_df['level'].astype('category')
+
+# %%
+
+# join reviews table on subject_id
+
+rtemp = reviews_df.rename({'created_at': 'review_dt'}, axis=1).drop(['assignment_id', 'spaced_repetition_system_id',
+                                                                     'data_updated_at', 'hour', 'day', 'month', 'year',
+                                                                     'weekday'], axis=1)
+rtemp['total_incorrect'] = rtemp['incorrect_meaning_answers'] + rtemp['incorrect_reading_answers']
+
+rs_df = rtemp.merge(subject_df.drop('data_updated_at', axis=1), left_on='subject_id',
+                    right_on='id', how='inner').drop('id', axis=1)
 
 
 # %%
